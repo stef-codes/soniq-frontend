06cdd745ed01f260b73ce401718d93b5248f2168
Stefanie <stefcodes@gmail.com>
2020-03-11 13:10:33 -0700

    connect props and add routing

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import {connect} from 'react-redux'; 
+import {getSongs} from './actions/songs'
+import HomePage from './components/HomePage/HomePage';
+import Playlists from './components/Playlists/Playlists';
+import Search from './components/Search'
 
-class App extends React.Component {
 
-  componentDidMount() {
-    fetch('http://localhost:3000/songs')
-    .then(response => response.json())
-    .then(song => console.log(song))
+  class App extends React.Component  {
+    
+    render() {
+      return (
+      <div>
+        <Router>
+          <Route exact path={"/"} component={HomePage} />
+          <Route exact path={"/playlists"} component={Playlists} />
+          <Route exact path={"/search"} component={Search} />
+        </Router>
+      </div>
+      )
+    }
   }
 
-  render () {
 
-    return (
-      <div className="App">
-        App
-      </div>
-    );
 
+const mapStateToProps = state => {
+  console.log("I am state", state)
+  return {
+    songs: state.songReducer.songs, 
+    loading: state.songReducer.loading
   }
 }
 
-export default App;
+export default connect(mapStateToProps, {getSongs})(App);
