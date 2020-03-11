5879fc6176ee1388c15f114bf3c0d7de275c7699
Stefanie <stefcodes@gmail.com>
2020-03-11 13:11:55 -0700

    review changes

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {CreateStore, applyMiddleware} from 'redux'; 
+import {createStore, applyMiddleware} from 'redux'; 
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk'; 
 import {Provider} from 'react-redux'; 
 import rootReducer from './reducers/rootReducer'
 import App from './App';
 
-const store = CreateStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>
+        <Provider store={store}>
+            <App />
+        </Provider>
 
 
 , document.getElementById('root'));
+
+
