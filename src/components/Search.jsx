ea1134903fc0aaf8db5813033e32663d60c1f92b
Stefanie <stefcodes@gmail.com>
2020-03-11 13:46:50 -0700

    change search component to a class component

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 // import '../search.styles.css'
 
-const Search = () => {
-    return (
-    <h1>Search for a Song to Add to a Playlist</h1>
-    )
+class Search extends React.Component {
+    render () {
+        return (
+            <h1>Search for a Song to Add to a Playlist</h1>
+    
+    
+            // Form 
+        )
+    }
 }
     
 export default Search
