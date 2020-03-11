3f62353ba06cd008b84ad3b8f81198fd8c099d62
Stefanie <stefcodes@gmail.com>
2020-03-11 13:56:46 -0700

    populate songs component

---
+++ src/components/Songs/Songs.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+// import '../songs.styles.css'
+
+const Songs = () => {
+    return (
+    <h1>Check out all the songs in current Playlists</h1>
+    )
+}
+    
+export default Songs
