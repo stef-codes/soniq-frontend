0d54af1eef3a11cbd4a05f99e6916ad71a39cd91
Stefanie <stefcodes@gmail.com>
2020-03-11 13:16:20 -0700

    add a loading action

--- src/reducers/songReducer.js
+++ src/reducers/songReducer.js
@@ -1,5 +1,10 @@
 export default (state = {songs: [], loading:false}, action) => {
     switch(action.type){
+        case "LOADING_SONGS":
+            return {
+                ...state, 
+                loading: true
+            }
         default:
             return state 
     }     
