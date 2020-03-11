a04d87ccc5c26be9da747ec2972186de35eda600
Stefanie <stefcodes@gmail.com>
2020-03-11 13:13:07 -0700

    create search component

---
+++ src/components/Search.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+// import '../search.styles.css'
+
+const Search = () => {
+    return (
+    <h1>Search for a Song to Add to a Playlist</h1>
+    )
+}
+    
+export default Search
