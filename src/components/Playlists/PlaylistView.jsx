3f8c8d4a40df6769cb09a72fecac011cac69c174
Stefanie <stefcodes@gmail.com>
2020-03-13 00:38:45 -0700

    pass playlist prop to playlist view

--- src/components/Playlists/PlaylistView.jsx
+++ src/components/Playlists/PlaylistView.jsx
@@ -1,39 +1,24 @@
 import React from 'react'
-// import '../playlists.styles.css'
-import {getPlaylists} from '../../actions/playlist'
-import { connect } from 'react-redux'
-import { Link } from 'react-router-dom'
+import Search from '../../containers/Search'
+// // import '../playlistview.styles.css'
+// import { connect } from 'react-redux'
+// import { Link } from 'react-router-dom'
 
-class PlaylistView extends React.Component {
-
-    render() {
-        const playlists = this.props.playlists.map((playlist) => 
+const PlaylistView = (props) => {
+    const playlist = props.location.state.playlist
+          return(
             <div>
-             <br/>
-            <li key={i}>{playlist.name}</li>
-            <Link to="/search">
-                <button type="button">
-                    Add a Song
-                </button>
-            </Link>
+                <h1>{playlist.name}</h1>
 
+
+                <h3>Add a Song to this Playlist</h3>
+                <Search key={playlist.id}/>
             </div>
          )
-        return(
-            <div>
-                <h2>Playlist Name</h2>
-            {playlists}
-            </div>
-        )
-    }
+    
 }
 
-const mapStateToProps = state => {
-    console.log("Playlist State", state)
-    return {
-      playlists: state.playlistReducer.playlists, 
-      loading: state.playlistReducer.loading
-    }
-  }
-    
-export default connect (mapStateToProps, {getPlaylists}) (PlaylistView)
+
+
+
+export default PlaylistView
