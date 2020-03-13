3f863c425dc860fe85f019063589cd6f69ead635
Stefanie <stefcodes@gmail.com>
2020-03-12 17:05:58 -0700

    display existing playlists on the page

--- src/components/Playlists/Playlists.jsx
+++ src/components/Playlists/Playlists.jsx
@@ -1,18 +1,32 @@
 import React from 'react'
 // import '../playlists.styles.css'
+import {getPlaylists} from '../../actions/playlist'
 import PlaylistForm from './PlaylistForm'
+import { connect } from 'react-redux'
 
 class Playlists extends React.Component {
-
+    componentDidMount(){
+        this.props.getPlaylists()
+    }
     render() {
-    return (
-    
-    <div>
-    <h1>See All Playlists</h1>
-
-        <PlaylistForm/>  
-    </div>
-    )}
+        const playlists = this.props.playlists.map((playlist, i) => <li key={i}>{playlist.name}</li>
+        )
+        return(
+            <div>
+                <h2>Your playlists</h2>
+                <ul>{this.props.loading ? <h3>...loading playlists</h3> : playlists} </ul>
+                <PlaylistForm/>
+            </div>
+        )
+    }
 }
+
+const mapStateToProps = state => {
+    console.log("Playlist State", state)
+    return {
+      playlists: state.playlistReducer.playlists, 
+      loading: state.playlistReducer.loading
+    }
+  }
     
-export default Playlists
+export default connect (mapStateToProps, {getPlaylists}) (Playlists)
