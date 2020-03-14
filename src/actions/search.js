b0a7b9e06e88fe19628a8c7b45a51301cad80fc3
Stefanie <stefcodes@gmail.com>
2020-03-13 23:22:07 -0700

    separate search action by type

--- src/actions/search.js
+++ src/actions/search.js
@@ -1,10 +1,21 @@
-export const getSearch = (musicArtist, musicTrack) => {
+export const getArtistSearch = (searchTerm) => {
     return dispatch => {
-        dispatch({type: "LOADING_SEARCH"})
-        return fetch(`https://itunes.apple.com/search?term=${musicArtist}&entity=${musicTrack}`)
+        dispatch({type: "LOADING_ARTIST_SEARCH"})
+        return fetch(`https://itunes.apple.com/search?term=${searchTerm}&entity=musicArtist`)
         .then(resp => resp.json())
         .then(data => {
-            dispatch({type: "SEARCH_SUCCESS", payload: data})
+            dispatch({type: "ARTIST_SEARCH_SUCCESS", payload: data})
+        })
+    }
+}
+
+export const getSongSearch = (searchTerm) => {
+    return dispatch => {
+        dispatch({type: "LOADING_SONG_SEARCH"})
+        return fetch(`https://itunes.apple.com/search?term=${searchTerm}&entity=musicTrack`)
+        .then(resp => resp.json())
+        .then(data => {
+            dispatch({type: "SONG_SEARCH_SUCCESS", payload: data})
         })
     }
 }
