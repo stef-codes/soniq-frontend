44e5c684a1aa89dcf4f3c8a444ba08feccb77081
Stefanie <stefcodes@gmail.com>
2020-03-13 23:23:57 -0700

    add searc container to search folder

---
+++ src/containers/search/SearchContainer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'; 
+import Search from './Search';
+
+class SearchContainer extends React.Component {
+    render() {
+        return (
+            <div>
+                <Search/>
+            </div>
+        );
+    }
+}
+
+export default SearchContainer;
