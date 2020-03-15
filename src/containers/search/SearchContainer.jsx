bb37be4d37219abccd2e786ff90ea7a54b6bd34c
Stefanie <stefcodes@gmail.com>
2020-03-14 20:03:47 -0700

    add logic to SearchContainer

--- src/containers/search/SearchContainer.jsx
+++ src/containers/search/SearchContainer.jsx
@@ -1,14 +1,29 @@
-import React from 'react'; 
-import Search from './Search';
+// import React from 'react'; 
+// import {connect} from 'react-redux';
+// import {getArtistSearch, getSongSearch} from '../../actions/search'
+// import Search from './Search';
 
-class SearchContainer extends React.Component {
-    render() {
-        return (
-            <div>
-                <Search/>
-            </div>
-        );
-    }
-}
+// class SearchContainer extends React.Component {
+//     render() {
+//         return (
+//             <div>
+//                 <Search/>
+//             </div>
+//         );
+//     }
+// }
 
-export default SearchContainer;
+// const mapStateToProps = (state) => (
+//     {
+//       result: state.search,
+//     }
+//   );
+//   const mapDispatchToProps = (dispatch) => (
+//     {
+//       search(searchTerm) {
+//         dispatch(search(searchTerm))
+//       },
+//     }
+//   );
+
+// export default connect(mapStateToProps, mapDispatchToProps)(SearchContainer);
