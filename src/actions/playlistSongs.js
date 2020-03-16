b2c4a5859d935af60b9908f4d34165902badfdbb
Stefanie <stefcodes@gmail.com>
2020-03-16 12:31:31 -0700

    create song before adding to playlist

--- src/actions/playlistSongs.js
+++ src/actions/playlistSongs.js
@@ -1,6 +1,22 @@
 export const addPlaylistSong = playlistSong => {
     return dispatch => {
-        dispatch({type: "ADD_PLAYLIST_SONG"})
+
+        dispatch({type: "ADD_SONG"})
+        return fetch(`http://localhost:3001/songs`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accepts': 'application/json'
+            },
+            body: JSON.stringify(song)
+        })
+        .then(resp => resp.json())
+        .then(data => {
+            dispatch({type: "ADD_SONG", payload: data})
+        })
+
+        .then(data => {
+            (dispatch({type: "ADD_PLAYLIST_SONG"})
         return fetch(`http://localhost:3001/playlist_songs`, {
             method: 'POST',
             headers: {
@@ -12,6 +28,6 @@ export const addPlaylistSong = playlistSong => {
         .then(resp => resp.json())
         .then(data => {
             dispatch({type: "PLAYLIST_SONG_ADDED", payload: data})
-        })
+        }))
     }
 }
